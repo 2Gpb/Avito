958c4a1832ed034a25d82a4f647447128d05dfef
2Gpb <Pvtyukhmenev@gmail.com>
2025-02-10 22:38:12 +0300

    create project

---
+++ TestTaskAvito/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TestTaskAvito
+//
+//  Created by Peter on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
