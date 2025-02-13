5e6c36640bea7a967389d8f98e37acc182939a8b
2Gpb <Pvtyukhmenev@gmail.com>
2025-02-13 18:52:47 +0300

    add tabbar

---
+++ TestTaskAvito/Application/TabBar/MainTabBarController.swift
@@ -0,0 +1,82 @@
+//
+//  MainTabBarController.swift
+//  TestTaskAvito
+//
+//  Created by Peter on 13.02.2025.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+    // MARK: - Constants
+    private enum Constant {
+        enum Search {
+            static let title: String = "Search"
+            static let imageName: String = "square.grid.2x2.fill"
+            static let tag: Int = 1
+        }
+        
+        enum Cart {
+            static let title: String = "Cart"
+            static let imageName: String = "cart.fill"
+            static let tag: Int = 2
+        }
+    }
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+    
+    // MARK: - SetUp
+    private func setUp() {
+        setUpAppearance()
+        setUpControllers()
+    }
+    
+    private func setUpAppearance() {
+        tabBar.backgroundColor = UIColor(color: .base60)
+        tabBar.tintColor = UIColor(color: .lightBlue)
+        tabBar.unselectedItemTintColor = UIColor(color: .base20)
+        tabBar.isTranslucent = false
+        
+        UITabBarItem
+            .appearance()
+            .setTitleTextAttributes([.font: TextStyle.tabSmall.font],for: .normal)
+    }
+    
+    private func setUpControllers() {
+        let searchViewController = createNavController(
+            rootViewController: SearchAssembly.build(),
+            title: Constant.Search.title,
+            imageName: Constant.Search.imageName,
+            tag: Constant.Search.tag
+        )
+        
+        let cartViewController = createNavController(
+            rootViewController: UIViewController(),
+            title: Constant.Cart.title,
+            imageName: Constant.Cart.imageName,
+            tag: Constant.Cart.tag
+        )
+        
+        setViewControllers([searchViewController, cartViewController], animated: false)
+    }
+    
+    func createNavController(
+        rootViewController: UIViewController,
+        title: String,
+        imageName: String,
+        tag: Int
+    ) -> UINavigationController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem = UITabBarItem(
+            title: title,
+            image: UIImage(systemName: imageName),
+            tag: tag
+        )
+        
+        return navController
+    }
+}
