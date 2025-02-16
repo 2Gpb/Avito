7f594aba8af16c11d3deb52dc710e7762de68348
2Gpb <Pvtyukhmenev@gmail.com>
2025-02-16 11:45:14 +0300

    add image cache

---
+++ TestTaskAvito/Core/Cache/ImageCache.swift
@@ -0,0 +1,36 @@
+//
+//  ImageCache.swift
+//  TestTaskAvito
+//
+//  Created by Peter on 16.02.2025.
+//
+
+import UIKit
+
+final class ImageCache {
+    // MARK: - Singleton
+    static let shared: ImageCache = ImageCache()
+    
+    // MARK: - Private fields
+    private let cache: NSCache = NSCache<NSString, UIImage>()
+    private let queue: DispatchQueue = DispatchQueue(label: "com.upnews.imageCache")
+    
+    // MARK: - Methods
+    func setImage(image: UIImage, forkey key: String) {
+        queue.async {
+            self.cache.setObject(image, forKey: key as NSString)
+        }
+    }
+    
+    func getImage(forKey key: String) -> UIImage? {
+        queue.sync {
+            cache.object(forKey: key as NSString)
+        }
+    }
+    
+    func clearCache() {
+        queue.async {
+            self.cache.removeAllObjects()
+        }
+    }
+}
